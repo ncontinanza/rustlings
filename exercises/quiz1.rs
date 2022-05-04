4696a2150f3a286c16771237240167f3c05a2fdc
nicolas.continanza <nicolas.continanza@almundo.com>
2022-05-03 23:42:29 -0300

    Advances up to Quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,10 +8,14 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_apple_price {
+ pub fn calculate_apple_price(amount: i32) -> i32 {
+    let mut price = 2;
+    if amount > 40 {
+        price = 1;
+    }
+    price * amount
+ }
 
 // Don't modify this function!
 #[test]
